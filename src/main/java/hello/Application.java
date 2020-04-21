bac96384503e7927001eb1ba2aeacf10e4220d25
tsharon-byte <sharonova_t@inbox.ru>
2020-04-21 13:54:23 +0300

    Добавлено логирование

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,5 +1,7 @@
 package hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -7,8 +9,11 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan
 @EnableAutoConfiguration
 public class Application {
+    private static final Logger logger
+            = LoggerFactory.getLogger(PersonController.class);
 
     public static void main(String[] args) {
+        logger.info("!!!DEBUG: started application");
         SpringApplication.run(Application.class, args);
     }
 
